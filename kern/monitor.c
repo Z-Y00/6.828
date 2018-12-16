246f235feec4132f766ee06462fb0162930c2b68
GengYu Rao <zouyoo@outlook.com>
2018-12-16 12:22:04 +0800

    clean the comments

--- kern/monitor.c
+++ kern/monitor.c
@@ -58,7 +58,6 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
 	uint32_t ebp,eip,args[5];
 	struct Eipdebuginfo debuginfo;
 	cprintf("Stack backtrace:\n");
